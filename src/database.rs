ed950b3ffd8811b9266c257afac8a1d520f9d4cd
MohanSharmav <catchmohansharma@gmail.com>
2023-04-21 11:25:02 +0900

    daatabase seprate file

--- src/database.rs
+++ src/database.rs
@@ -1,6 +1,41 @@
 
-use sqlx::{Error, PgPool, Pool, Postgres, Row};
-use tokio::select;
+use sqlx::{Error, Row};
+use sqlx::postgres::PgPoolOptions;
+
+
+
+pub(crate) async fn selecting() -> Result<(),Error>{
+    // let rows = sqlx::query("SELECT name FROM categories")
+    //     .map(|row:sqlx::postgres::PgRow|{
+    //          let a:String=row.get("name");
+    //       println!("{:?}",rows)
+    //     })
+    //     .fetch_all(pool)
+    //     .await?;
+
+
+    dotenv::dotenv().expect("Unable to load environment variables from .env file");
+
+    let db_url = std::env::var("DATABASE_URL").expect("Unable to read DATABASE_URL env var");
+
+    let mut pool = PgPoolOptions::new()
+        .max_connections(100)
+        .connect(&db_url)
+        .await.expect("Unable to connect to Postgres");
+
+
+    println!("⭐⭐⭐⭐⭐⭐");
+    let  rows = sqlx::query("SELECT name FROM categories")
+        .fetch_all(&pool)
+        .await?;
+
+    for row in rows{
+        let name: String=row.get("name");
+        println!("name is ⭐: {}",name)
+    }
+    Ok(())
+}
+
 
 // async fn select_all_from_table(pool: &Pool<Postgres>) -> anyhow::Result<Vec<(i32, String)>> {
 //      // let rows = sqlx::query("SELECT title,description,name FROM posts")
@@ -16,22 +51,3 @@ use tokio::select;
 //
 //      Ok(results)
 // }
-
-async fn select(pool: &PgPool)-> Result<(),Error>{
-    // let rows = sqlx::query("SELECT name FROM categories")
-    //     .map(|row:sqlx::postgres::PgRow|{
-    //          let a:String=row.get("name");
-    //       println!("{:?}",rows)
-    //     })
-    //     .fetch_all(pool)
-    //     .await?;
-    let  rows = sqlx::query("SELECT name FROM categories")
-        .fetch_all(pool)
-        .await?;
-
-    for row in rows{
-        let name: String=row.get("name");
-        println!("name is : {}",name)
-    }
-    Ok(())
-}
